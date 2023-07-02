535d1b64eb1a2497324f5540248732a760e11312
Andrew Fletcher <afletcher168@gmail.com>
2023-07-02 00:25:23 -0700

    Updated solver api and backend to make it more useful

--- m3l/core/m3l_classes.py
+++ m3l/core/m3l_classes.py
@@ -9,27 +9,36 @@ from lsdo_modules.module_csdl.module_csdl import ModuleCSDL
 from lsdo_modules.module.module import Module
 
 
-@dataclass
-class Node:
-    '''
-    name : str
-        The name of the node.
-    '''
-    name : str
+# @dataclass
+# class Node:
+#     '''
+#     name : str
+#         The name of the node.
+#     '''
+#     name : str
 
-@dataclass
-class Operation(Node):
-    '''
-    An M3L operation. This represents a mapping/model/operation/tranformation in the overall model.
+# @dataclass
+# class Operation(Node):
+#     '''
+#     An M3L operation. This represents a mapping/model/operation/tranformation in the overall model.
 
-    Parameters
-    ----------
-    name : str
-        The name of the variable.
-    arguments : dict[str:Variable]
-        The dictionary of Variables that are arguments to the operation. The key is the string name of the variable in the operation.
-    '''
-    arguments : dict
+#     Parameters
+#     ----------
+#     name : str
+#         The name of the variable.
+#     arguments : dict[str:Variable]
+#         The dictionary of Variables that are arguments to the operation. The key is the string name of the variable in the operation.
+#     '''
+#     arguments : dict
+
+
+class Operation(Module):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.assign_attributes()  # Added this to make code more developer friendly (more familiar looking)
+        
+    def assign_attributes(self):
+        pass
 
 
 @dataclass
@@ -50,7 +59,13 @@ class CSDLOperation(Operation):
     operation_csdl : csdl.Model
 
 
-class ExplicitOperation(Module):
+class ExplicitOperation(Operation):
+
+    def assign_attributes(self):
+        '''
+        Assigns class attributes to make class more like standard python class.
+        '''
+        pass
     
     def compute(self):
         '''
@@ -138,6 +153,68 @@ class Variable:
 #     operation : Operation = None
 #     value : np.ndarray = None
 
+class Add(ExplicitOperation):
+
+    def initialize(self, kwargs):
+        pass
+    
+    def compute(self):
+        '''
+        Creates the CSDL model to compute the function evaluation.
+
+        Returns
+        -------
+        csdl_model : {csdl.Model, lsdo_modules.ModuleCSDL}
+            The csdl model or module that computes the model/operation outputs.
+        '''
+        x1 = self.arguments['x1']
+        x2 = self.arguments['x2']
+
+        operation_csdl = csdl.Model()
+        x1_csdl = operation_csdl.declare_variable(name='x1', shape=x1.shape)
+        x2_csdl = operation_csdl.declare_variable(name='x2', shape=x2.shape)
+        y = x1_csdl + x2_csdl
+        output_name = f'{x1.name}_plus_{x2.name}'
+        operation_csdl.register_output(name=output_name, var=y)
+        return operation_csdl
+
+    def compute_derivates(self):
+        '''
+        -- optional --
+        Creates the CSDL model to compute the derivatives of the model outputs. This is only needed for dynamic analysis.
+        For now, I would recommend coming back to this.
+
+        Returns
+        -------
+        derivatives_csdl_model : {csdl.Model, lsdo_modules.ModuleCSDL}
+            The csdl model or module that computes the derivatives of the model/operation outputs.
+        '''
+        pass
+
+    def evaluate(self, x1:Variable, x2:Variable) -> Variable:
+        '''
+        User-facing method that the user will call to define a model evaluation.
+
+        Parameters
+        ----------
+        mesh : Variable
+            The mesh over which the function will be evaluated.
+
+        Returns
+        -------
+        function_values : Variable
+            The values of the function at the mesh locations.
+        '''
+        self.name = f'{x1.name}_plus_{x2.name}_operation'
+
+        # Define operation arguments
+        self.arguments = {'x1' : x1, 'x2' : x2}
+
+        # Create the M3L variables that are being output
+        function_values = Variable(name=f'{x1.name}_plus_{x2.name}', shape=x1.shape, operation=self)
+        return function_values
+
+
 
 @dataclass
 class FunctionSpace:
@@ -183,30 +260,34 @@ class Function:
         function_values : FunctionValues
             A variable representing the evaluated function values.
         '''
-        num_values = np.prod(mesh.shape[:-1])
-        num_coefficients = np.prod(self.coefficients.shape[:-1])
-        temp_map = np.eye(num_values, self.function_space.num_coefficients)
-        temp_map[self.function_space.num_coefficients:,0] = np.ones((num_values-self.function_space.num_coefficients,))
-        output_name = f'nodal_{self.name}'
-        output_shape = tuple(mesh.shape[:-1]) + (self.coefficients.shape[-1],)
+        # num_values = np.prod(mesh.shape[:-1])
+        # num_coefficients = np.prod(self.coefficients.shape[:-1])
+        # temp_map = np.eye(num_values, self.function_space.num_coefficients)
+        # temp_map[self.function_space.num_coefficients:,0] = np.ones((num_values-self.function_space.num_coefficients,))
+        # output_name = f'nodal_{self.name}'
+        # output_shape = tuple(mesh.shape[:-1]) + (self.coefficients.shape[-1],)
 
-        # csdl_map = csdl.Model()
-        csdl_map = ModuleCSDL()
-        function_coefficients = csdl_map.register_module_input(self.coefficients.name, shape=(num_coefficients, self.coefficients.shape[-1]))
-        map_csdl = csdl_map.create_input(f'{self.name}_evaluation_map', temp_map)
-        flattened_function_values_csdl = csdl.matmat(map_csdl, function_coefficients)
-        function_values_csdl = csdl.reshape(flattened_function_values_csdl, new_shape=output_shape)
-        csdl_map.register_output(output_name, function_values_csdl)
+        # # csdl_map = csdl.Model()
+        # csdl_map = ModuleCSDL()
+        # function_coefficients = csdl_map.register_module_input(self.coefficients.name, shape=(num_coefficients, self.coefficients.shape[-1]))
+        # map_csdl = csdl_map.create_input(f'{self.name}_evaluation_map', temp_map)
+        # flattened_function_values_csdl = csdl.matmat(map_csdl, function_coefficients)
+        # function_values_csdl = csdl.reshape(flattened_function_values_csdl, new_shape=output_shape)
+        # csdl_map.register_output(output_name, function_values_csdl)
 
-        # parametric_coordinates = self.function_space.reference_geometry.project(mesh.value, return_parametric_coordinates=True)
-        # map = self.function_space.compute_evaluation_map(parametric_coordinates)
+        # # parametric_coordinates = self.function_space.reference_geometry.project(mesh.value, return_parametric_coordinates=True)
+        # # map = self.function_space.compute_evaluation_map(parametric_coordinates)
 
-        # Idea: Have the MappedArray store the parametric coordinates so we don't have to repeatendly perform projection.
+        # # Idea: Have the MappedArray store the parametric coordinates so we don't have to repeatendly perform projection.
 
-        # function_values = Variable(name=output_name, upstream_variables={self.name:self}, map=csdl_map, mesh=mesh)
+        # # function_values = Variable(name=output_name, upstream_variables={self.name:self}, map=csdl_map, mesh=mesh)
 
-        evaluate_operation = CSDLOperation(name=f'{self.name}_evaluation', arguments={self.coefficients.name: self.coefficients}, operation_csdl=csdl_map)
-        function_values = Variable(name=output_name, shape=output_shape, operation=evaluate_operation)
+        # evaluate_operation = CSDLOperation(name=f'{self.name}_evaluation', arguments={self.coefficients.name: self.coefficients}, operation_csdl=csdl_map)
+        # function_values = Variable(name=output_name, shape=output_shape, operation=evaluate_operation)
+        # return function_values
+
+        function_evaluation_model = FunctionEvaluation(function=self, mesh=mesh)
+        function_values = function_evaluation_model.evaluate(self.coefficients)
         return function_values
     
     def inverse_evaluate(self, function_values:Variable):
@@ -235,6 +316,85 @@ class Function:
         operation = CSDLOperation(name=f'{self.name}_inverse_evaluation', arguments=[function_values], operation_csdl=csdl_map)
         function_coefficients = Variable(name=f'{self.name}_coefficients', shape=coefficients_shape, operation=operation)
         return function_coefficients
+
+
+class FunctionEvaluation(ExplicitOperation):
+    def initialize(self, kwargs):
+        self.parameters.declare('function', types=Function)
+        self.parameters.declare('mesh', types=am.MappedArray)
+
+    def assign_attributes(self):
+        '''
+        Assigns class attributes to make class more like standard python class.
+        '''
+        self.function = self.parameters['function']
+        self.mesh = self.parameters['mesh']
+    
+    def compute(self):
+        '''
+        Creates the CSDL model to compute the function evaluation.
+
+        Returns
+        -------
+        csdl_model : {csdl.Model, lsdo_modules.ModuleCSDL}
+            The csdl model or module that computes the model/operation outputs.
+        '''
+
+        mesh = self.mesh
+        function_space = self.function.function_space
+        coefficients = self.arguments['coefficients']
+
+        num_values = np.prod(mesh.shape[:-1])
+        num_coefficients = np.prod(coefficients.shape[:-1])
+        temp_map = np.eye(num_values, function_space.num_coefficients)
+        temp_map[function_space.num_coefficients:,0] = np.ones((num_values-function_space.num_coefficients,))
+        output_name = f'evaluated_{self.function.name}'
+        output_shape = tuple(mesh.shape[:-1]) + (coefficients.shape[-1],)
+
+        csdl_map = ModuleCSDL()
+        function_coefficients = csdl_map.register_module_input(coefficients.name, shape=(num_coefficients, coefficients.shape[-1]))
+        map_csdl = csdl_map.create_input(f'{self.name}_evaluation_map', temp_map)
+        flattened_function_values_csdl = csdl.matmat(map_csdl, function_coefficients)
+        function_values_csdl = csdl.reshape(flattened_function_values_csdl, new_shape=output_shape)
+        csdl_map.register_output(output_name, function_values_csdl)
+        return csdl_map
+
+    def compute_derivates(self):
+        '''
+        -- optional --
+        Creates the CSDL model to compute the derivatives of the model outputs. This is only needed for dynamic analysis.
+        For now, I would recommend coming back to this.
+
+        Returns
+        -------
+        derivatives_csdl_model : {csdl.Model, lsdo_modules.ModuleCSDL}
+            The csdl model or module that computes the derivatives of the model/operation outputs.
+        '''
+        pass
+
+    def evaluate(self, coefficients:Variable) -> tuple:
+        '''
+        User-facing method that the user will call to define a model evaluation.
+
+        Parameters
+        ----------
+        mesh : Variable
+            The mesh over which the function will be evaluated.
+
+        Returns
+        -------
+        function_values : Variable
+            The values of the function at the mesh locations.
+        '''
+        self.name = f'{self.function.name}_evaluation'
+
+        # Define operation arguments
+        self.arguments = {'coefficients' : coefficients}
+
+        # Create the M3L variables that are being output
+        function_values = Variable(name=f'evaluated_{self.function.name}', shape=self.mesh.shape, operation=self)
+        return function_values
+
 
 
 class Model:   # Implicit (or not implicit?) model groups should be an instance of this
@@ -302,12 +462,39 @@ class Model:   # Implicit (or not implicit?) model groups should be an instance 
     def gather_operations(self, variable:Variable):
         if variable.operation is not None:
             operation = variable.operation
-            for arg_name, arg in operation.arguments.items():
-                self.gather_operations(arg)
+            for input_name, input in operation.arguments.items():
+                self.gather_operations(input)
 
             if operation.name not in self.operations:
                 self.operations[operation.name] = operation
 
+
+    # def assemble(self):
+    #     # Assemble output states
+    #     for output_name, output in self.outputs.items():
+    #         self.gather_operations(output)
+        
+    #     model_csdl = ModuleCSDL()
+
+    #     for operation_name, operation in self.operations.items():   # Already in correct order due to recursion process
+
+    #         if type(operation.operation_csdl) is csdl.Model:
+    #             model_csdl.add(submodel=operation.operation_csdl, name=operation.name, promotes=[]) # should I suppress promotions here?
+    #         else: # type(operation.operation_csdl) is ModuleCSDL:
+    #             model_csdl.add_module(submodule=operation.operation_csdl, name=operation.name, promotes=[]) # should I suppress promotions here?
+            
+
+    #         for arg_name, arg in operation.arguments.items():
+    #             if arg.operation is not None:
+    #                 model_csdl.connect(arg.operation.name+"."+arg.name, operation_name+"."+arg_name)  # Something like this for no promotions
+    #                 # if arg.name == arg_name:
+    #                 #     continue    # promotion will automatically connect if the names match
+    #                 # else:
+    #                 #     model_csdl.connect(arg.name, arg_name)  # If names don't match, connect manually
+
+    #     self.csdl_model = model_csdl
+    #     return self.csdl_model
+    
 
     def assemble(self):
         # Assemble output states
@@ -318,25 +505,26 @@ class Model:   # Implicit (or not implicit?) model groups should be an instance 
 
         for operation_name, operation in self.operations.items():   # Already in correct order due to recursion process
 
-            if type(operation.operation_csdl) is csdl.Model:
-                model_csdl.add(submodel=operation.operation_csdl, name=operation.name, promotes=[]) # should I suppress promotions here?
-            else: # type(operation.operation_csdl) is ModuleCSDL:
-                model_csdl.add_module(submodule=operation.operation_csdl, name=operation.name, promotes=[]) # should I suppress promotions here?
-            
+            if issubclass(type(operation), ExplicitOperation):
+                operation_csdl = operation.compute()
 
-            for arg_name, arg in operation.arguments.items():
-                if arg.operation is not None:
-                    model_csdl.connect(arg.operation.name+"."+arg.name, operation_name+"."+arg_name)  # Something like this for no promotions
-                    # if arg.name == arg_name:
-                    #     continue    # promotion will automatically connect if the names match
-                    # else:
-                    #     model_csdl.connect(arg.name, arg_name)  # If names don't match, connect manually
+                if type(operation_csdl) is csdl.Model:
+                    model_csdl.add(submodel=operation_csdl, name=operation_name, promotes=[]) # should I suppress promotions here?
+                elif issubclass(type(operation_csdl), ModuleCSDL):
+                    model_csdl.add_module(submodule=operation_csdl, name=operation_name, promotes=[]) # should I suppress promotions here?
+                else:
+                    raise Exception(f"{operation.name}'s compute() method is returning an invalid model type.")
+
+                for input_name, input in operation.arguments.items():
+                    if input.operation is not None:
+                        model_csdl.connect(input.operation.name+"."+input.name, operation_name+"."+input_name) # when not promoting
+
 
         self.csdl_model = model_csdl
         return self.csdl_model
 
 
-    def _assemble_csdl(self):
+    def assemble_csdl(self) -> ModuleCSDL:
         self.assemble()
 
         return self.csdl_model
--- m3l/core/m3l_functions.py
+++ m3l/core/m3l_functions.py
@@ -4,19 +4,8 @@ def add(x1, x2):
     '''
     Performs addition with between two M3L variables.
     '''
-    # x1_name = x1.name
-    # x2_name = x2.name
-    # if x2 == x1:
-    #     x2_name = f'{x2_name}_'
-
-    operation_csdl = csdl.Model()
-    x1_csdl = operation_csdl.declare_variable(name=x1.name, shape=x1.shape)
-    x2_csdl = operation_csdl.declare_variable(name=x2.name, shape=x2.shape)
-    y = x1_csdl + x2_csdl
-    output_name = f'{x1.name}_plus_{x2.name}'
-    operation_csdl.register_output(name=output_name, var=y)
 
     import m3l
-    arguments = {x1.name:x1, x2.name:x2}
-    csdl_operation = m3l.CSDLOperation(name=f'{output_name}_operation', arguments=arguments, operation_csdl=operation_csdl)
-    return m3l.Variable(name=output_name, shape=y.shape, operation=csdl_operation)
+    addition_operation = m3l.Add()
+
+    return addition_operation.evaluate(x1, x2)
